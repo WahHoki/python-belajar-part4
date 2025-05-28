56cf1b9800f9ade2ce0f32f2ac8a0743641c0b4f
Hoki <wahhoki07@gmail.com>
2025-05-28 17:56:27 +0800

    Create 6.pengenalanfungsi.py

---
+++ 6.pengenalanfungsi.py
@@ -0,0 +1,19 @@
+# fungsi di python adalah intepreted language
+# membuat fungsi
+
+
+def hello_world():
+       # fungsi menampilkan hello world
+       print("Hello World")
+       print("ucup surucup")
+       print("otongers")
+
+hello_world()
+hello_world()
+
+
+def fungsi():
+       # pemanggilan fungsi harus setelah dibuat
+       print("ini adalah fungsi")
+
+fungsi()
